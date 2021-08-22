77f21b0905260291a2be95feda78607575ac3ef3
SamanthaBooth81 <samanthabooth09@live.co.uk>
2021-08-22 17:20:43 +0000

    add to structure of script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 /**
  * The main game loop which is called when a game first starts 
  */
-function runGame() {
-
-    document.getElementById("random-number").value= "";
-    // Generates the random number the user will make their guess from
+function runGame(randomNum) {
+    // Generates the random number the user will make their guesses from
     let randomNumber = Math.floor(Math.random) * 25;  
 }
 
 function checkAnswer()
 
 function calculateCorrectAnswer()
+
+function incrementScore()
