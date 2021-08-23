9f1665ec3ad14e1cdfdcf966b01420b29e003572
SamanthaBooth81 <samanthabooth09@live.co.uk>
2021-08-23 23:21:59 +0000

    Add event listener to buttons, change element for 'press play' to a button and gix css on button, add data-type to button elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,40 @@
-let c = 0;
-function pop() {
-    document.getElementById("play").addEventListener("click");
+// let c = 0;
+// function pop() {
+//     document.getElementById("play").addEventListener("click");
     
-    if (c == 0) {
-        document.getElementById("box").style.display = "block";
-        c = 1;
-    } else {
-            document.getElementById("box").style.display = "none";  
-            c = 0;
+//     if (c == 0) {
+//         document.getElementById("box").style.display = "block";
+//         c = 1;
+//     } else {
+//             document.getElementById("box").style.display = "none";  
+//             c = 0;
+//         }
+// }
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            if(this.getAttribute("data-type") === "play") {
+                alert("You clicked play");
+            } else if (this.getAttribute("data-type") === "higher") {
+                alert(`You clicked ${higher}`);
+            } else {
+                let lower = this.getAttribute("data-type") === "lower";
+                alert(`You clicked ${lower}`);
+            }
         }
-}
+    }
+})
 
 function runGame() {
+}
+
+function displayRandomNumber(){
+
     // Generates the random number the user will make their guesses from
     let randomNumber = Math.floor(Math.random) * 25;  
+
 }
 
 function checkAnswer()
