b20a5a4d08fcc5a27439e5e69f6e4edd6f6cb35c
SamanthaBooth81 <samanthabooth09@live.co.uk>
2021-08-23 00:24:29 +0000

    add pop out box for when user clicks play, javascript code to be completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
-/**
- * The main game loop which is called when a game first starts 
- */
-function runGame(randomNum) {
+function pop() {
+    document.getElementById("play").addEventListener("click");
+    }
+
+    let c = 0;
+
+    if (c == 0) {
+        document.getElementById("box").style.display = "block";
+        c = 1;
+    } else {
+            document.getElementById("box").style.display = "none";  
+            c = 0;
+        }
+
+function runGame() {
     // Generates the random number the user will make their guesses from
     let randomNumber = Math.floor(Math.random) * 25;  
 }
@@ -11,3 +22,5 @@ function checkAnswer()
 function calculateCorrectAnswer()
 
 function incrementScore()
+
+
