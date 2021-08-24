df1273cf11e2f07233b03b1334c5e5ee681b2272
SamanthaBooth81 <samanthabooth09@live.co.uk>
2021-08-24 18:07:35 +0000

    removed commented out code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,9 @@
-// let c = 0;
-// function pop() {
-//     document.getElementById("play").addEventListener("click");
-    
-//     if (c == 0) {
-//         document.getElementById("box").style.display = "block";
-//         c = 1;
-//     } else {
-//             document.getElementById("box").style.display = "none";  
-//             c = 0;
-//         }
-// }
-
 document.addEventListener("DOMContentLoaded", function() {
 
     let buttons = document.getElementsByTagName("button");
     for (let button of buttons) {
         button.addEventListener("click", function(){
             if(this.getAttribute("data-type") === "play") {
-                // alert("You clicked play");
                 onclick = document.getElementById("random-number").innerHTML = generateNumber;
             } else if (this.getAttribute("data-type") === "higher") {
                 alert("You clicked higher");
